6d9f6b021d37942567db1177b0740f27ac760ad4
abhishekwl <nateriver210@gmail.com>
2019-03-17 20:13:58 +0530

    Added Notification Support

---
+++ app/src/main/java/letswave/co/in/wave/Services/FirebaseInstanceService.java
@@ -0,0 +1,25 @@
+package letswave.co.in.wave.Services;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+import java.util.Objects;
+
+public class FirebaseInstanceService extends FirebaseInstanceIdService {
+    private static final String TAG="FirebaseInstanceService";
+
+    @Override
+    public void onTokenRefresh() {
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        FirebaseMessaging.getInstance().subscribeToTopic("all");
+        Log.v(TAG, "Refreshed token: " + refreshedToken);
+        sendRegistrationToServer(Objects.requireNonNull(refreshedToken));
+    }
+
+    private void sendRegistrationToServer(String refreshedToken) {
+        Log.v("TOKEN ", refreshedToken);
+    }
+}
